ecb86447a01eb2cf6cb97b5b5c0fe7eb8cbb0380
8954sood <8954sood@naver.com>
2024-05-18 00:19:35 +0900

    chore: Rename Theme

--- app/src/main/java/com/plass/travling/ui/theme/Theme.kt
+++ app/src/main/java/com/plass/travling/ui/theme/Theme.kt
@@ -1,26 +1,19 @@
 package com.plass.travling.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 
 @Composable
-fun TravlingTheme(
+fun TravelingTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -35,14 +28,14 @@ fun TravlingTheme(
         }
     }
     CompositionLocalProvider(
-        LocalTravelingColor provides GrowTheme.colorScheme,
-        LocalTravelingTypography provides GrowTheme.typography,
+        LocalTravelingColor provides TravelingTheme.colorScheme,
+        LocalTravelingTypography provides TravelingTheme.typography,
         content = content
     )
 }
 
 
-object GrowTheme {
+object TravelingTheme {
     val colorScheme: TravelingColor
         @Composable
         @ReadOnlyComposable
