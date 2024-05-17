ab6b3b6af3831a981ef6455cfffe6a5b758ef67c
8954sood <8954sood@naver.com>
2024-05-18 00:08:25 +0900

    chore: Add Color

--- app/src/main/java/com/plass/travling/ui/theme/TravelingColor.kt
+++ app/src/main/java/com/plass/travling/ui/theme/TravelingColor.kt
@@ -6,6 +6,9 @@ import androidx.compose.ui.graphics.Color
 object TravelingColor {
     val Black = Color(0xFF000000)
     val White = Color(0xFFFFFFFF)
+    val Blue = Color(0xFF0078F9)
+    val Gray = Color(0xFFCCCCD6)
+    val Transparent = Color(0x00000000)
 }
 
 val LocalTravelingColor = compositionLocalOf { TravelingColor }
