5832715a84b2cf6ed6682ddd04107715621c9245
8954sood <8954sood@naver.com>
2024-05-18 05:29:30 +0900

    chore: Locate Item Width To Full

--- app/src/main/java/com/plass/travling/ui/feature/locate/LocateItem.kt
+++ app/src/main/java/com/plass/travling/ui/feature/locate/LocateItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -36,6 +37,7 @@ fun LocateItem(
 ) {
     Box(
         modifier = Modifier
+            .fillMaxWidth()
             .rippleClickable(onClick = onClickItem)
     ) {
         Column(
