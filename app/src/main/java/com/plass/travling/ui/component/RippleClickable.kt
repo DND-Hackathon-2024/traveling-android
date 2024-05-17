0688e47a81f3488f5da3981f634e6b48969b6eda
8954sood <8954sood@naver.com>
2024-05-18 00:29:54 +0900

    feat: Create Ripple Clickable

---
+++ app/src/main/java/com/plass/travling/ui/component/RippleClickable.kt
@@ -0,0 +1,19 @@
+package com.plass.travling.ui.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+fun Modifier.rippleClickable(
+    enabled: Boolean,
+    onClick: () -> Unit,
+) = composed { this.clickable(
+    onClick = onClick,
+    enabled = enabled,
+    interactionSource = remember { MutableInteractionSource() },
+    indication = rememberRipple()
+) }
